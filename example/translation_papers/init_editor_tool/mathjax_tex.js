743d547a73f5b1b3588f55ce39d3deaac35c22d4
official business <wx_9e4c73a607f54399aa3916dbd1109e9f@git.code.tencent.com>
2022-06-21 20:40:49 +0800

    继续完善中英文对照案例，简单版，完善mathjax操作

--- example/translation_papers/init_editor_tool/mathjax_tex.js
+++ example/translation_papers/init_editor_tool/mathjax_tex.js
@@ -3,51 +3,104 @@ export default function initMathjax(editorEnglish, editorChinese){
 	let
 		mathjaxButton = document.getElementById('mathjaxButton'),
 		mathjaxContainer = document.getElementById('mathjaxContainer'),
-		mathjaxImg = document.getElementById('mathjaxImg'),
-		mathjaxTex = document.getElementById('mathjaxTex'),
+		imgConatienr = mathjaxContainer.getElementsByClassName('img')[0],
+		// mathjaxImg = document.getElementById('mathjaxImg'),
+		mathjaxTexContainer = document.getElementById('mathjaxTexContainer'),
+		// mathjaxTex = document.getElementById('mathjaxTex'),
 		comfirm = document.getElementById('comfirm'),
 		cancel = document.getElementById('cancel')
 
 
 	mathjaxButton.onmousedown = showMathjax;
 
-	mathjaxTex.oninput = function(e){
-		mathjaxImg.src = getBase64(mathjaxTex.value)
-	}
-	mathjaxTex.onkeydown = function(e){
-		// console.log('e:', e);
-		if( e.key === 'Enter' ){
-			e.preventDefault();
-			comfirmMathJax();
-		}else if( e.key === 'Escape' ){
-			hiddenMathjax();
-		}
-	}
 	cancel.onclick = hiddenMathjax
 	comfirm.onclick = comfirmMathJax
 
 	editorEnglish.addComponentEvent('mathjax', 'imgMousedown', (edtior, mathjaxObj)=>{
 		if( edtior === editorEnglish ){
 			showMathjax();
-			mathjaxTex.value = mathjaxObj.data;
+			// console.log('mathjaxObj.data:', Array.isArray(mathjaxObj.data));
+			if( Array.isArray(mathjaxObj.data) ){
+				initMathjaxTex(mathjaxObj.data.length);
+				for( let i = 0, len = mathjaxObj.data.length; i < len; i++ ){
+					// console.log('mathjaxTexContainer.childNodes[i]:', mathjaxTexContainer.childNodes[i]);
+					mathjaxTexContainer.childNodes[i].value = mathjaxObj.data[i];
+				}
+			}else{
+				mathjaxTexContainer.childNodes[0].value = mathjaxObj.data;
+			}
+			initMathjaxTexImage();
 		}
 	});
 
+	function initMathjaxTexImage(){
+		editorEnglish.nodeApi.emptyAllChild(imgConatienr);
+		console.log('imgConatienr:', imgConatienr);
+		imgConatienr.appendChild(editorChinese.getComponentDom({
+			type: 'mathjax',
+			data: getMathjaxTexValue()
+		}));
+	}
+
+	function initMathjaxTex(num = 1){
+		editorEnglish.nodeApi.emptyAllChild(mathjaxTexContainer);
+		for( let i = 0; i < num; i++ ){
+			addMathjaxTex();
+		}
+	}
+
+	function addMathjaxTex(){
+		let mathjaxTex = document.createElement('textarea');
+		mathjaxTex.className = 'mathjaxTex';
+
+		mathjaxTex.oninput = function(e){
+			initMathjaxTexImage();
+		}
+		mathjaxTex.onkeydown = function(e){
+			// console.log('e:', e);
+			if( e.key === 'Enter' ){
+				e.preventDefault();
+				comfirmMathJax();
+			}else if( e.key === 'Escape' ){
+				hiddenMathjax();
+			}
+		}
+		mathjaxTexContainer.appendChild(mathjaxTex);
+	}
+
+	function getMathjaxTexValue(){
+		let tex ;
+		if( mathjaxTexContainer.childNodes.length > 1 ){
+			tex = [];
+			for( let i = 0, len = mathjaxTexContainer.childNodes.length; i < len; i++ ){
+				let mathjaxTex = mathjaxTexContainer.childNodes[i];
+				if( mathjaxTex.value && mathjaxTex.value.trim() ){
+					tex.push(mathjaxTex.value);
+				}
+			}
+		}else{
+			tex = mathjaxTexContainer.childNodes[0].value;
+		}
+		return tex;
+	}
+
 	function comfirmMathJax(){
 		hiddenMathjax();
-		if( mathjaxTex.value && mathjaxTex.value.trim() ){
+		let tex = getMathjaxTexValue();
+		if( tex ){
 			let range = editorChinese.getRange();
 			editorChinese.insertElement( editorChinese.getComponentDom({
 				type: 'mathjax',
-				data: mathjaxTex.value
+				data: tex
 			}), range.startContainer, range.startOffset );
 		}
 	}
 
 	function showMathjax(){
+		initMathjaxTex(1)
 		mathjaxContainer.style.display = 'flex';
 		setTimeout(()=>{
-			mathjaxTex.focus();
+			mathjaxTexContainer.childNodes[0].focus();
 		}, 0);
 	}
 	function hiddenMathjax(){
